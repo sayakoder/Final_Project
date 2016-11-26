5f7ee9f3da1a1bc85049e7c5b53f2a70842e178e
wawan8989 <wawan.ke5.666@gmail.com>
2016-11-26 10:20:11 +0700

    pembaruan title

--- application/config/database.php
+++ application/config/database.php
@@ -98,32 +98,33 @@ $db['default'] = array(
 	'stricton' => FALSE,
 	'failover' => array(),
 	'save_queries' => TRUE
+	);
 
 // ______________________________________offline
 // ______________________________________offline
 // ______________________________________offline
 // ______________________________________offline
-	// $active_group = 'default';
-	// $query_builder = TRUE;
+// $active_group = 'default';
+// $query_builder = TRUE;
 
-	// $db['default'] = array(
-	// 	'dsn'	=> '',
-	// 	'hostname' => 'localhost',
-	// 	'username' => 'root',
-	// 	'password' => '',
-	// 	'database' => 'cendana_wawan_finalproject',
-	// 	'dbdriver' => 'mysqli',
-	// 	'dbprefix' => '',
-	// 	'pconnect' => FALSE,
-	// 	'db_debug' => (ENVIRONMENT !== 'production'),
-	// 	'cache_on' => FALSE,
-	// 	'cachedir' => '',
-	// 	'char_set' => 'utf8',
-	// 	'dbcollat' => 'utf8_general_ci',
-	// 	'swap_pre' => '',
-	// 	'encrypt' => FALSE,
-	// 	'compress' => FALSE,
-	// 	'stricton' => FALSE,
-	// 	'failover' => array(),
-	// 	'save_queries' => TRUE
-		);
+// $db['default'] = array(
+// 	'dsn'	=> '',
+// 	'hostname' => 'localhost',
+// 	'username' => 'root',
+// 	'password' => '',
+// 	'database' => 'cendana_wawan_finalproject',
+// 	'dbdriver' => 'mysqli',
+// 	'dbprefix' => '',
+// 	'pconnect' => FALSE,
+// 	'db_debug' => (ENVIRONMENT !== 'production'),
+// 	'cache_on' => FALSE,
+// 	'cachedir' => '',
+// 	'char_set' => 'utf8',
+// 	'dbcollat' => 'utf8_general_ci',
+// 	'swap_pre' => '',
+// 	'encrypt' => FALSE,
+// 	'compress' => FALSE,
+// 	'stricton' => FALSE,
+// 	'failover' => array(),
+// 	'save_queries' => TRUE
+// 	);
